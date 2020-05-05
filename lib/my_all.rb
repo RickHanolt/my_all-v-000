dbbf0b392ea14397bf6d641b73f15a6a487703ac
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 20:59:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,16 +1,29 @@
 require 'pry'
 
-def my_all?(array)
+def my_all?(collection)
   i = 0
-  while i < array.size
-
-    if yield(array[i])
+  while i < collection.size
+    if yield(collection[i])
       i += 1
-      if i == array.size
+      if i == collection.size
         return true
       end
     else
       return false
     end
+  end
+end
+
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
